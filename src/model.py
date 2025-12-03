b2a9ee50809fc977e3e44f9057d5b57796111da5
Alibubere <alibubere989@gmail.com>
2025-12-03 17:03:04 +0530

    freeze backbone

--- src/model.py
+++ src/model.py
@@ -3,21 +3,31 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 def get_model(num_classes):
-    
+
     model = maskrcnn_resnet50_fpn(weights="DEFAULT")
+
+    # Replace box predictor
     in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features,num_classes)
-
+    # Replace mask predictor
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-
     hidden_layers = 256
-
     model.roi_heads.mask_predictor = MaskRCNNPredictor(
         in_features_mask,
         hidden_layers,
-        num_classes,
+        num_classes
     )
 
+    model.train()  # IMPORTANT
+
     return model
+
+def freeze_backbone(model):
+    
+    for param in model.backbone.parameters():
+        param.requires_grad = False
+
+    return model 
+
 
