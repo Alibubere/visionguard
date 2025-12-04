1be75378e6a1cfa7385d3c716d0e2ee20a94ef72
Alibubere <alibubere989@gmail.com>
2025-12-04 12:50:49 +0530

    logging setup

---
+++ main.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+def setup_logging():
+    """
+    Set Up logging for the Entire Pipeline 
+    """
+
+    log_dir = "logs"
+    os.makedirs(log_dir,exist_ok=True)
+    log_file = os.path.join(log_dir,"pipeline.log")
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format= "%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[logging.FileHandler(log_file),logging.StreamHandler()],
+    )
+    logging.info("Logging successfully Initialized")
+
+
+
+def main():
+    setup_logging()
+    
+
+
+if __name__ == "__main__":
+
+    main()
